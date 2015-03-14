c38a47a26fbd0bc887d960a96e1b4aba09e42c1e
Bram Van Mensel <vanmensel.bram@gmail.com>
2015-03-14 01:38:03 +0100

    Started building mode

---
+++ Assets/thrust.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var thrust: float;
+var rb: Rigidbody;
+
+function Start() {
+	rb = GetComponent.<Rigidbody>();
+}
+
+function FixedUpdate () {
+	if (Input.GetKey ("return")){
+	rb.AddForce(transform.up * thrust);
+	}
+}
+
+function Update () {
+
+}
