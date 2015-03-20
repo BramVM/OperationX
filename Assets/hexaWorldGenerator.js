32a1f91a0f0137ecfe70ce92155985b7633d0a3f
Nick Mingneau <nick.mingneau@progreso.be>
2015-03-20 17:41:19 +0100

    hexaWorldGenerator.js adjustments

--- Assets/hexaWorldGenerator.js
+++ Assets/hexaWorldGenerator.js
@@ -1,41 +1,33 @@
 ﻿#pragma strict
 
-public var grid_x = 100;
-public var grid_z = 173.205080757;
+public var grid_x : float = 100;
+var grid_z = Mathf.Sqrt((Mathf.Pow((grid_x*2), 2)-Mathf.Pow((grid_x), 2)));
 
 function Start () {
-/*1.73205080757*/
+
+/*get player position*/
 var playerPos_x = GameObject.Find("Main Camera").transform.position.x;
 var playerPos_z = GameObject.Find("Main Camera").transform.position.z;
 
-/*var scale = 100; current scale is 100, make relative if necessary*/
+/*instantiate position of new hexagon*/
+var newHexPos_x : float;
+var newHexPos_z : float;
 
-var newHexPos_x : int;
-var newHexPos_z : int;
- 
-var pos_xRest = playerPos_x % grid_x;   
-var pos_zRest = playerPos_z % grid_z; 
+/*get amount of gridlines between player position and origin*/
+var gridNumber_x : float = playerPos_x / grid_x;
+var gridNumber_z : float = playerPos_z / grid_z;
 
-var gridNumber_x = playerPos_x / 100;
-var gridNumber_z = playerPos_z / 100;
-
-/*rounding doesnt work --> because floats are possible*/
-
-if(pos_xRest <=50){
-	newHexPos_x = Mathf.Round(gridNumber_x) * grid_x;
-}
-else{
-	newHexPos_x = Mathf.Round(gridNumber_x) * grid_x;
-}
-
-if(pos_xRest <= (grid_z/2)){
-	newHexPos_z = Mathf.Round(gridNumber_z) * grid_z;
-}
-else{
-	newHexPos_z = Mathf.Round(gridNumber_z) * grid_z;
-}
+/*round the amount of gridlines to get the closest hexagon origin*/
+newHexPos_x = Mathf.Round(gridNumber_x) * grid_x;
+newHexPos_z = Mathf.Round(gridNumber_z) * grid_z;
 
 Debug.Log(newHexPos_x);
+Debug.Log(newHexPos_z);
+
+/*Debug.Log(GameObject.Find("hexagon").bounds.size.x);
+
+/*var cube : GameObject  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+cube.transform.position = Vector3(newHexPos_x, 0.5, newHexPos_z);*/
 }
 
 function Update () {
