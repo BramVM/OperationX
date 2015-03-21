8ea9a1b03bb5ff85ed6f975d7e4d0ba82beb036c
Bram Van Mensel <vanmensel.bram@gmail.com>
2015-03-21 17:02:01 +0100

    Buildscript Update

---
+++ Assets/Scripts/screenSize.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+ 
+     var height = Camera.main.orthographicSize * 2.0;
+     var width = height * Screen.width / Screen.height;
+     //width=width-100;
+     transform.localScale = Vector3(width, 1, 1);
+ }
