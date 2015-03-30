cd594fe80c8b6e51055313b067b57075bcd3218f
Bram <bram.vanmensel@progreso.be>
2015-03-30 19:04:28 +0200

    update thruster

--- Assets/Scripts/thrust.js
+++ Assets/Scripts/thrust.js
@@ -1,24 +1,31 @@
 ﻿#pragma strict
-var thrust: float;
-var rb: Rigidbody;
-@script RequireComponent(Light);
+public var thrust: float;
+public var thruster: GameObject;
+public var thrustLight: GameObject;
 
-var minIntensity = 3; var maxIntensity = 10;
+private var minIntensity = 3;
+private var maxIntensity = 10;
 private var random : float;
+private var light : GameObject;
 random = Random.Range(3,10);
 
 function Start() {
-	rb = GetComponent.<Rigidbody>();
-	/*GetComponent.<thrust_animation>().Play();*/
+	thrustLight = Instantiate(thrustLight,thruster.transform.position,thruster.transform.rotation)as GameObject;
+	thrustLight.transform.SetParent(thruster.transform,false);
+	thrustLight.transform.position.y=thrustLight.transform.position.y+0.7;
 }
 
 function FixedUpdate () {
-	GetComponent.<Light>().intensity =0;
+	thrustLight.transform.position=thruster.transform.position;
+	thrustLight.transform.position.y=thrustLight.transform.position.y+0.7;
+	thrustLight.transform.rotation=thruster.transform.rotation;
+	thrustLight.transform.rotation.eulerAngles.x=thrustLight.transform.rotation.x+90;
+	thrustLight.GetComponent.<Light>().intensity =0;
 	if (Input.GetKey ("return")){
-		rb.AddForce(-transform.up * thrust);
+		thruster.GetComponent.<Rigidbody>().AddForce(-transform.up * thrust);
 		//Debug.Log(thrust);
 		var noise = Mathf.PerlinNoise(random, Time.time*10);
-		GetComponent.<Light>().intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+		thrustLight.GetComponent.<Light>().intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
 	}
 }
 
